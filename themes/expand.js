2b3d4d425dc269302b4227e33367cfc6ecd1f7b8
igserver <pavel.petrzela@internetguru.cz>
2018-06-28 20:10:01 +0200

    gituserfiles autocommit

--- themes/expand.js
+++ themes/expand.js
@@ -1,10 +1,27 @@
 (function(){
   var descs = document.querySelectorAll('.clanek .desc');
+  
+   var multiClickHandler = function (handlers, delay) {
+    var clicks = 0, timeout, delay = delay || 250;
+    return function (e) {
+      clicks++;
+      clearTimeout(timeout);
+      timeout = setTimeout(function () {
+        if(handlers[clicks]) handlers[clicks](e);
+        clicks = 0;
+      }, delay);
+    };
+  }
+  
   for (var i = 0; i < descs.length; i++) {
-    descs[i].addEventListener("click", function (e) {
-      e.target.classList.toggle("expand");
-      return false;
-      e.preventDefault();
-    }, false);
+    var handler = multiClickHandler({
+      2: function (e) {
+        e.target.classList.toggle("expand");
+        return false;
+        e.preventDefault();
+      }
+    });
+    descs[i].addEventListener("touchend", handler, false);
+    descs[i].addEventListener("click", handler, false);
   }
 })()
