1cb87a31d2a59879e76d28f966d5178ba1962359
igserver <pavel.petrzela@internetguru.cz>
2018-07-01 21:10:02 +0200

    gituserfiles autocommit

--- themes/expand.js
+++ themes/expand.js
@@ -1,35 +1,61 @@
 (function(){
   var descs = document.querySelectorAll('.clanek .desc');
   
-   var multiClickHandler = function (handlers, delay) {
-    var clicks = 0, timeout, delay = delay || 250;
+  var fullDescs = [];
+  
+  var multiClickHandler = function (handlers, index, delay) {
+    var clicks = 0, timeout, index = index, delay = delay || 250;
     return function (e) {
       clicks++;
       clearTimeout(timeout);
       timeout = setTimeout(function () {
-        if(handlers[clicks]) handlers[clicks](e);
+        if(handlers[clicks]) handlers[clicks](e, index);
         clicks = 0;
       }, delay);
     };
   }
+   
+  function shorten (str, maxLen, separator) {
+    if (typeof separator === 'undefined') {
+      separator = ' ';
+    }
+    var len = str.length;
+    if (len <= maxLen) {
+      return str;
+    }
+    var indexof = str.lastIndexOf(separator, maxLen);
+    if (len - indexof < 10) {
+      return str;
+    }
+    return str.substr(0, indexof) + '...';
+  }
   
   for (var i = 0; i < descs.length; i++) {
     var handler = multiClickHandler({
-      2: function (e) {
-        /*
-        if (e.target.classList.contains("expand")) {
-          e.target.title = "Rozbalit dvojklikem"
-        } else {
-          e.target.title = "Sbalit dvojklikem"
-        }
-        */
+      1: function (e, index) {
         e.target.classList.toggle("expand");
+        if (e.target.classList.contains("expand")) {
+          e.target.innerText = fullDescs[index];
+//           e.target.title = "Rozbalit dvojklikem"
+        } else {
+//           e.target.title = "Sbalit dvojklikem"
+          e.target.innerText = shorten(fullDescs[index], 285);
+        }
         e.preventDefault();
         return false;
       }
-    });
-    descs[i].addEventListener("touchend", handler, false);
-    descs[i].addEventListener("click", handler, false);
+    }, i);
+    fullDescs.push(descs[i].innerText);
+    var short = shorten(descs[i].innerText, 285);
+    if (short == descs[i].innerText.length) {
+      continue;
+    }
+    descs[i].innerText = short;
+    var button = document.createElement("a");
+    button.innerText = ">";
+    descs[i].appendChild(button);
+    button.addEventListener("touchend", handler, false);
+    button.addEventListener("click", handler, false);
     // descs[i].title = "Rozbalit dvojklikem"
   }
 })()
