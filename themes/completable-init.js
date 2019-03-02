2e67340099412b91a072c6d9afebdbf8a76fc8c1
admin <igcms@commit.com>
2019-03-02 12:02:36 +0000

    Admin

--- themes/completable-init.js
+++ themes/completable-init.js
@@ -2,7 +2,7 @@
   require("IGCMS.Completable", function() {
     IGCMS.Completable.init({
       selectSelector: '#header select.completable',
-      placeholder: "Filtrovat články (Ctrl+Shift+F)",
+      placeholder: "Co Vás zajímá? (Ctrl+Shift+F)",
       defaultChangeText: "Změnit filtr",
       filterSelector: ".filter",
       sendFormText: "Hledat na Google",
