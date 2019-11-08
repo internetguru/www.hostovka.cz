d87a98dd64ef631ced0641aea8c77d21c3d246cb
example <you@example.com>
2019-11-08 15:46:54 +0100

    update getLabels

--- getLabels.sh
+++ getLabels.sh
@@ -1,37 +1,13 @@
 #!/bin/bash
-echo "--- Unique labels ---"
 for f in plugins/Agregator/clanky/*.html; do
-  hxselect -c "body > h + desc::attr(kw)" < "$f" \
-    | tr "," $'\n' \
-    | sed 's/^ *\| *$//'
-  echo
-done | sort -u
-
-echo
-echo "--- link-labels ---"
-for f in plugins/Agregator/clanky/*.html; do
-  #id="$(hxselect -c "body > h::attr(id)" < "$f")"
+  cnt=0
+  fname="${f##*/}"
+  fname="${fname%.html}"
+  id="$(hxselect -c "body > h::attr(id)" < "$f")"
+  ctime="$(hxselect -c "body > h::attr(ctime)" < "$f")"
+  mtime="$(hxselect -c "body > h::attr(mtime)" < "$f")"
   while read -r kw; do
-    fname="${f##*/}"
-    fname="${fname%.html}"
-    echo -e "$fname;$kw"
+    echo -e "$id;$fname;$kw;$ctime;$mtime"
   done <<< "$(hxselect -c "body > h + desc::attr(kw)" < "$f" | tr "," $'\n' | sed 's/^ *\| *$//')"
 done
-
-echo
-echo "--- filenames ---"
-for f in plugins/Agregator/clanky/*.html; do
-  fname="${f##*/}"
-  echo "${fname%.html}"
-done
-
-echo
-echo "--- mtimes ---"
-for f in $(grep -l "mtime=" plugins/Agregator/clanky/*.html); do
-  hxselect -c "body > h::attr(id)" < "$f"
-  echo -n ";"
-  hxselect -c "body > h::attr(mtime)" < "$f"
-  echo
-done
-
 
