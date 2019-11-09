98f032c6ec8cef08c6d71b786f95b6a306b1a487
example <you@example.com>
2019-11-09 19:26:34 +0100

    user_udpatekw.sh

--- getLabels.sh
+++ getLabels.sh
@@ -6,6 +6,8 @@ for f in plugins/Agregator/clanky/*.html; do
   id="$(hxselect -c "body > h::attr(id)" < "$f")"
   ctime="$(hxselect -c "body > h::attr(ctime)" < "$f")"
   mtime="$(hxselect -c "body > h::attr(mtime)" < "$f")"
+  [[ -z "$mtime" ]] \
+    && mtime="$ctime"
   kw="$(hxselect -c "body > h + desc::attr(kw)" < "$f")"
   echo -e "$id;$fname;$kw;$ctime;$mtime"
 done
