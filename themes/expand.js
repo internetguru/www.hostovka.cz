187d8df3074ada705fac9e3f1c23565598393a73
ov2428 <info@internetguru.cz>
2020-03-01 16:05:53 +0100

    fix docinfo, expand

--- themes/expand.js
+++ themes/expand.js
@@ -3,15 +3,14 @@
   
   var fullDescs = [];
   
-  var multiClickHandler = function (handlers, index, button, delay) {
-    var clicks = 0, timeout, index = index, button = button, delay = delay || 250;
+  var multiClickHandler = function (handlers, index, button) {
+    var clicks = 0, timeout, index = index, button = button;
     return function (e) {
       clicks++;
-      clearTimeout(timeout);
-      timeout = setTimeout(function () {
-        if(handlers[clicks]) handlers[clicks](e, index, button);
-        clicks = 0;
-      }, delay);
+      if (handlers[clicks]) {
+        handlers[clicks](e, index, button)
+      }
+      clicks = 0;
     };
   }
    
@@ -41,7 +40,7 @@
           button.innerText = "› ‹";
           button.title = "Sbalit"
         } else {
-          parent.innerText = shorten(fullDescs[index], 195);
+          parent.innerText = shorten(fullDescs[index], 150);
           button.innerText = "‹ ›";
           button.title = "Rozbalit"
         }
@@ -51,7 +50,7 @@
       }
     }, i, button);
     fullDescs.push(descs[i].innerText);
-    var short = shorten(descs[i].innerText, 195);
+    var short = shorten(descs[i].innerText, 150);
     if (short.length == descs[i].innerText.length) {
       continue;
     }
