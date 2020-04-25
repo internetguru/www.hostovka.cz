ddfb874aa649d372f2404d9790d5bb914562dfa9
pavel <igcms@commit.com>
2020-04-25 10:09:47 +0000

    Admin

--- themes/moreableinit.js
+++ themes/moreableinit.js
@@ -4,4 +4,9 @@ require("IGCMS.Moreable", function () {
     moreText: "Další články kategorie",
     leftText: "zbývá %s"
   })
+  IGCMS.Moreable.init({
+    parent: ".dalsi + .list-wrapper--multiple > div",
+    moreText: "Další příbuzné články",
+    leftText: "zbývá %s"
+  })
 })
