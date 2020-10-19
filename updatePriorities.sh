5043a99eab39c6e4f7db1a46c89871fbd8cd8c9d
ov2428 <info@internetguru.cz>
2020-10-19 10:19:22 +0200

    autocommit

--- updatePriorities.sh
+++ updatePriorities.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-cnt=1000
-declare -i i
 while read -r line; do
   link="$(echo "$line" | cut -d, -f1)"
   fname="plugins/Agregator/clanky/${link##/}.html"
+  views="$(echo "$line" | cut -d, -f2)"
   [[ -f "$fname" ]] \
     || continue
-  i+=1
-  priority=$(( cnt - i ))
+  echo "${views// /}"
   sed -i 's/data-top="[^"]\+" //' "$fname"
-  sed -i "s/ctime/data-top=\"$priority\" ctime/" "$fname"
+  sed -i "s/ctime/data-top=\"$views\" ctime/" "$fname"
 done < "analytics.csv"
 
