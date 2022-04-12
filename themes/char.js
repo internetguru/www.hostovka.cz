fee8576ee2a0a2604cd4852eb1214548923d1006
ov2428 <info@internetguru.cz>
2022-04-12 18:37:59 +0200

    Update seznam clanku

--- themes/char.js
+++ themes/char.js
@@ -1,10 +1,14 @@
 (function(win) {
   let ul = document.querySelector('ul.vsechny_clanky')
   let lastChar = '-1'
-  let chars = []
+  let chars = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
+  let usedChars = []
   Array.from(ul.children).forEach((item) => {
-    const char = item.innerText.charAt(0).toUpperCase()
+    const char = item.innerText.charAt(0).toUpperCase().normalize("NFD").replace(/[\u0300-\u036f]/g, "")
     if (char == lastChar) {
+      return
+    }
+    if (!char.match(/^[A-ZĚŠČŘŽÝÁÍÉÚŮŤŇĎ]$/)) {
       return
     }
     let elm = document.createElement('li')
@@ -13,13 +17,17 @@
     elm.className = 'first-char'
     item.parentNode.insertBefore(elm, item)
     lastChar = char
-    chars.push(char)
+    usedChars.push(char)
   })
   let wrapper = document.createElement('div')
   wrapper.className = 'chars'
   chars.forEach((item) => {
     let a = document.createElement('a')
-    a.href = `#${item}`
+    if (usedChars.includes(item)) {
+      a.href = `#${item}`
+    } else {
+      a.href = `#`
+    }
     a.innerText = item
     wrapper.appendChild(a)
   })
